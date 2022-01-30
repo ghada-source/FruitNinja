9b734e76441b2ced1c0ab960c6eea478cc25f93d
ghada-source <ghada.amairi@yahoo.fr>
2022-01-30 23:11:23 +0100

    petit modif

--- Assets/Scripts/ChoiseLevel.cs
+++ Assets/Scripts/ChoiseLevel.cs
@@ -1,12 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using MySql.Data.MySqlClient;
 using UnityEngine.UI;
+using System.IO;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+using System;
 
 public class ChoiseLevel : MonoBehaviour
 {
- 
+
+
     public static string level;
     public Button yourButton;
+    public loginSystem logi;
+
 
     void Start()
     {
@@ -36,6 +45,29 @@ public class ChoiseLevel : MonoBehaviour
         }
         
         Debug.Log("level =" + level);
+        // UPDATE `Users` SET `level`= '[value-5]' WHERE `pseudo`= 'ghada'
+        /*
+        logi.ConnectBDD();
+
+        string commandsql = "UPDATE `Users` SET `level` = '"+ level +"' WHERE 'pseudo' ='" + loginSystem.pseudo + "'";
+
+        MySqlCommand cmd = new MySqlCommand(commandsql, loginSystem.connec);
+        try
+        {
+            cmd.ExecuteReader();
+
+
+        }
+        catch (IOException Ex)
+        {
+
+            Debug.Log("ajout echouer");
+        }
+        //on met le level dans le bdd
+        cmd.Dispose();
+        loginSystem.con.Close();
+
+        */
         SceneManager.LoadScene("Prototype 5");
     }
 }
